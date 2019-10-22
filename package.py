1cadc6b7b2ce5ac9b3a686709b51287b4e48b5b4
Joshua Senouf <joshua.senouf@gmail.com>
2019-10-22 17:21:09 -0400

    Adding MIT license. Misc changes in the Bash scripts

--- package.py
+++ package.py
@@ -33,6 +33,7 @@ def commands():
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib64")
     env.PKG_CONFIG_PATH.prepend("{root}/lib64/pkgconfig")
+    env.CMAKE_MODULE_PATH.prepend("{root}/lib64/cmake/glfw3")
 
     # Helper environment variables.
     env.GLFW_INCLUDE_PATH.set("{root}/include")
