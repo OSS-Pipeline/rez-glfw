e4a9807cebc53724c78f188b771880e3311a00b4
Joshua Senouf <joshua.senouf@gmail.com>
2023-10-25 18:37:41 -0400

    Updated to GLFW 3.3.8

--- package.py
+++ package.py
@@ -1,40 +1,41 @@
 name = "glfw"
 
-version = "3.3.0"
+version = "3.3.8"
 
-authors = [
-    "GLFW Development Team"
-]
-
-description = \
-    """
+description = """
     GLFW is an Open Source, multi-platform library for OpenGL, OpenGL ES and Vulkan application development.
     It provides a simple, platform-independent API for creating windows, contexts and surfaces, reading input,
     handling events, etc.
     """
 
+authors = [
+    "GLFW Development Team",
+]
+
 requires = [
-    "cmake-3+",
-    "gcc-6+"
+    "cmake-3",
+    "gcc-4.8+",
 ]
 
 variants = [
-    ["platform-linux"]
+    [
+        "platform-linux",
+    ],
 ]
 
 build_system = "cmake"
 
+
 with scope("config") as config:
     config.build_thread_count = "logical_cores"
 
-uuid = "glfw-{version}".format(version=str(version))
 
 def commands():
     env.PATH.prepend("{root}/bin")
-    env.LD_LIBRARY_PATH.prepend("{root}/lib64")
-    env.PKG_CONFIG_PATH.prepend("{root}/lib64/pkgconfig")
-    env.CMAKE_MODULE_PATH.prepend("{root}/lib64/cmake/glfw3")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.PKG_CONFIG_PATH.prepend("{root}/lib/pkgconfig")
+    env.CMAKE_MODULE_PATH.prepend("{root}/lib/cmake/glfw3")
 
     # Helper environment variables.
     env.GLFW_INCLUDE_PATH.set("{root}/include")
-    env.GLFW_LIBRARY_PATH.set("{root}/lib64")
+    env.GLFW_LIBRARY_PATH.set("{root}/lib")
